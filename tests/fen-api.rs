c23f6ba6603a41666cabcf3505d9ba6928d1093b
Oliver Wooding <oliverwooding@icloud.com>
2023-11-17 23:35:47 +0000

    updated tests

--- tests/fen-api.rs
+++ tests/fen-api.rs
@@ -5,16 +5,13 @@ use konig::standard::piece::StandardPiece;
 
 #[test]
 fn check_apis() {
-    let index = StandardIndex::try_from(4u8).unwrap();
-    let fen_board =
-        FenData::try_from("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2")
-            .unwrap()
-            .as_board();
+    let index = StandardIndex::new(5);
+    let fen_board = FenData::default().as_board();
 
     let white_king = fen_board.get_piece_at(index.into());
 
     assert_eq!(
         StandardPiece::WhiteKing,
         white_king.unwrap().to_owned().into()
-    )
+    );
 }
